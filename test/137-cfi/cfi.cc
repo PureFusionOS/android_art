c127e1519c2e67d7ce9083d6fb8ac674fa512335
David Srbecky <dsrbecky@google.com>
2018-03-09 17:23:01 +0000

    Support unwinding though the switch interpreter
    Wrap the switch interpreter in small assembly method which defines
    DEX PC in CFI and thus it allows libunwind to backtrace through it.
    
    Bug: 22414682
    Test: testrunner.py --host -t 137
    Test: testrunner.py --target -t 137
    Change-Id: I31dad9f0fb446151baaa99234b64f25c8ca2fa87
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- test/137-cfi/cfi.cc
+++ test/137-cfi/cfi.cc
@@ -147,7 +147,6 @@ extern "C" JNIEXPORT jboolean JNICALL Java_Main_unwindInProcess(
   // "mini-debug-info" does not include parameters to save space.
   std::vector<std::string> seq = {
       "Java_Main_unwindInProcess",                   // This function.
-      "Main.unwindInProcess",                        // The corresponding Java native method frame.
       "int java.util.Arrays.binarySearch(java.lang.Object[], int, int, java.lang.Object, java.util.Comparator)",  // Framework method.
       "Main.main"                                    // The Java entry method.
   };
@@ -249,11 +248,7 @@ extern "C" JNIEXPORT jboolean JNICALL Java_Main_unwindOtherProcess(
     // See comment in unwindInProcess for non-exact stack matching.
     // "mini-debug-info" does not include parameters to save space.
     std::vector<std::string> seq = {
-        // "Java_Main_sleep",                        // The sleep function being executed in the
-                                                     // other runtime.
-                                                     // Note: For some reason, the name isn't
-                                                     // resolved, so don't look for it right now.
-        "Main.sleep",                                // The corresponding Java native method frame.
+        "Java_Main_sleep",                           // The sleep function in the other process.
         "int java.util.Arrays.binarySearch(java.lang.Object[], int, int, java.lang.Object, java.util.Comparator)",  // Framework method.
         "Main.main"                                  // The Java entry method.
     };
